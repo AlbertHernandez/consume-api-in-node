50d20318a4a90c4f990cf802c85aa0ffa50e48e4
Albert Hernandez <alberthernandezdev@gmail.com>
2022-08-25 13:42:04 +0200

    initial version
    Signed-off-by: Albert Hernandez <alberthernandezdev@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch';
+
+const run = async () => {
+    const response = await fetch('https://api.github.com/users/github');
+    const data = await response.json();
+
+    console.log(data);
+}
+
+run()
